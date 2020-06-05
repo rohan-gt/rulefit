5a513bf5f18ddc24774a3675d115b663cae85c93
Rohan George Thampi <rohan.thampi@fokal.ai>
2020-06-05 18:24:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='RuleFit',
       version='0.3',
       description='RuleFit algorithm',
-      author='Christoph Molnar',
-      author_email='christoph.molnar@gmail.com',
+      author='Rohan George Thampi',
+      author_email='rohangt1591@gmail.com',
       url='',
       packages=['rulefit'],
-      install_requires=['scikit-learn>=0.20.2',
-                        'numpy>=1.16.1',
-                        'pandas>=0.24.1']
-     )
+      install_requires=[
+            'scikit-learn>=0.20.2',
+            'numpy>=1.16.1',
+            'pandas>=0.24.1']
+)
